be5678e2845d070d0cc12ee0a4ce03b27984f840
Marius Quabeck <misterq@ubuntu.com>
2016-09-06 14:36:38 +0200

    Add rc-proposed channel for opo

---
+++ devices/oneplusone/ubuntu/rc-proposedwipe.sh
@@ -0,0 +1,52 @@
+clear
+echo ""
+echo "Flashing rc-proposed channel"
+echo ""
+sleep 1
+echo "Please boot your OnePlus One into fastboot mode by pressing Power & Volume Up (+)"
+echo ""
+sleep 1
+echo -n "Is your OnePlus One in fastboot mode now? [Y] "; read bootloadermode
+if [ "$bootloadermode"==Y -o "$bootloadermode==y" -o "$bootloadermode"=="" ]; then
+  clear
+  echo ""
+  echo "Detecting device"
+  echo ""
+  sleep 1
+  fastboot devices >~/.AttachedDevices
+fi
+  if grep 'device$\|fastboot$' ~/.AttachedDevices
+  then
+    echo "Device detected !"
+    sleep 1
+    clear
+    echo ""
+    echo "Flashing rc-proposed channel"
+    echo ""
+    sleep 1
+    fastboot format cache
+    fastboot format userdata
+    fastboot reboot-bootloader
+    sleep 6
+    echo ""
+    sudo ubuntu-device-flash --server=http://system-image.ubports.com touch --channel=ubuntu-touch/rc-proposed --device=bacon --bootstrap
+    echo ""
+    echo "Move to your device to finish the setup."
+    sleep 1
+    echo ""
+    echo "Cleaning up.."
+    rm -f ~/.AttachedDevices
+    echo ""
+    sleep 1
+    echo "Exiting script. Bye Bye"
+    sleep 1
+    exit
+    else
+    echo "Device not found"
+    rm -f ~/.AttachedDevices
+    sleep 1
+    echo ""
+    echo "Back to menu"
+    sleep 1
+    . ./launcher.sh
+fi

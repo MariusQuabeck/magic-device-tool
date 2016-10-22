28677d91baa04e3bac7d45ecb1e074bbf6de7f9d
Marius Quabeck <misterq@ubuntu.com>
2016-10-22 19:35:41 +0200

    Add Sailfish OS for Nexus 7 2013 flo

--- devices/nexus7/nexus7.sh
+++ devices/nexus7/nexus7.sh
@@ -11,23 +11,24 @@ echo ""
 sleep 1
 echo "Ubuntu: "
 echo ""
-echo "[1] Install Ubuntu"
-echo "[2] Install OpenStore"
-echo "[3] Screencast"
+echo "[1]  Install Ubuntu"
+echo "[2]  Install OpenStore"
+echo "[3]  Screencast"
 echo ""
 echo ""
 echo "Android: "
 echo ""
-echo "[4] Install Cyanogenmod 13"
-echo "[5] Install Cyanogenmod 13 without Gapps"
-echo "[6] Install Phoenix OS"
-echo "[7] Install Factory Image"
-echo "[8] Install TWRP recovery"
-echo "[9] Back Up your Android device"
+echo "[4]  Install Cyanogenmod 13"
+echo "[5]  Install Cyanogenmod 13 without Gapps"
+echo "[6]  Install Sailfish OS"
+echo "[7]  Install Phoenix OS"
+echo "[8]  Install Factory Image"
+echo "[9]  Install TWRP recovery"
+echo "[10] Back Up your Android device"
 echo ""
-echo "[10] Lock/Unlock bootloader (Will wipe existing apps/data)"
+echo "[11] Lock/Unlock bootloader (Will wipe existing apps/data)"
 echo ""
-echo "[11] Back to menu "
+echo "[12] Back to menu "
 echo ""
 echo -n "Enter option: "; read option
 if [ "$option" = "1" ]; then
@@ -41,16 +42,18 @@ elif [ "$option" = "4" ]; then
 elif [ "$option" = "5" ]; then
   . ./devices/nexus7/android/cyanogenmodwogapps.sh
 elif [ "$option" = "6" ]; then
-  . ./devices/nexus7/android/phoenixos.sh
+  . ./devices/nexus7/android/sailfishos.sh
 elif [ "$option" = "7" ]; then
-  . ./devices/nexus7/android/factoryimage.sh
+  . ./devices/nexus7/android/phoenixos.sh
 elif [ "$option" = "8" ]; then
-  . ./devices/nexus7/android/twrp.sh
+  . ./devices/nexus7/android/factoryimage.sh
 elif [ "$option" = "9" ]; then
-  . ./devices/generic/android/backup.sh
+  . ./devices/nexus7/android/twrp.sh
 elif [ "$option" = "10" ]; then
-  . ./devices/nexus7/android/bootloader.sh
+  . ./devices/generic/android/backup.sh
 elif [ "$option" = "11" ]; then
+  . ./devices/nexus7/android/bootloader.sh
+elif [ "$option" = "12" ]; then
   . ./launcher.sh
 else
   echo ""

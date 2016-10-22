05066d5de3778421f3cb4a0712789d4b39dfbc7a
Marius Quabeck <misterq@ubuntu.com>
2016-10-22 19:13:28 +0200

    Add Sailfish OS for Nexus 7 2012 deb

--- devices/nexus7deb/nexus7.sh
+++ devices/nexus7deb/nexus7.sh
@@ -11,21 +11,22 @@ echo ""
 sleep 1
 echo "Ubuntu: "
 echo ""
-echo "[1] Install Ubuntu"
-echo "[2] Install OpenStore"
+echo "[1]  Install Ubuntu"
+echo "[2]  Install OpenStore"
 echo ""
 echo ""
 echo "Android: "
 echo ""
-echo "[3] Install Cyanogenmod 13"
-echo "[4] Install Cyanogenmod 13 without Gapps"
-echo "[5] Install Factory Image"
-echo "[6] Install TWRP recovery"
-echo "[7] Back Up your Android device"
+echo "[3]  Install Cyanogenmod 13"
+echo "[4]  Install Cyanogenmod 13 without Gapps"
+echo "[5]  Install Sailfish OS"
+echo "[6]  Install Factory Image"
+echo "[7]  Install TWRP recovery"
+echo "[8]  Back Up your Android device"
 echo ""
-echo "[8] Lock/Unlock bootloader (Will wipe existing apps/data)"
+echo "[9]  Lock/Unlock bootloader (Will wipe existing apps/data)"
 echo ""
-echo "[9] Back to menu "
+echo "[10] Back to menu "
 echo ""
 echo -n "Enter option: "; read option
 if [ "$option" = "1" ]; then
@@ -37,14 +38,16 @@ elif [ "$option" = "3" ]; then
 elif [ "$option" = "4" ]; then
 . ./devices/nexus7deb/android/cyanogenmodwogapps.sh
 elif [ "$option" = "5" ]; then
-. ./devices/nexus7deb/android/factoryimage.sh
+. ./devices/nexus7deb/android/sailfishos.sh
 elif [ "$option" = "6" ]; then
-  . ./devices/nexus7deb/android/twrp.sh
+. ./devices/nexus7deb/android/factoryimage.sh
 elif [ "$option" = "7" ]; then
-  . ./devices/generic/android/backup.sh
+  . ./devices/nexus7deb/android/twrp.sh
 elif [ "$option" = "8" ]; then
-  . ./devices/nexus7deb/android/bootloader.sh
+  . ./devices/generic/android/backup.sh
 elif [ "$option" = "9" ]; then
+  . ./devices/nexus7deb/android/bootloader.sh
+elif [ "$option" = "10" ]; then
   . ./launcher.sh
 else
   echo ""

84b05bb40c9b467a7c9c69c54b92f95eb700edbd
Marius Quabeck <misterq@ubuntu.com>
2017-01-26 14:39:53 +0100

    Update launcher

--- launcher.sh
+++ launcher.sh
@@ -112,7 +112,7 @@ echo ""
 sleep 1
 echo -n "Enter number: "; read device
 if [ "$device" = "1" ]; then
-  .$SNAP/devices/e45/e45.sh
+   .$SNAP/devices/e45/e45.sh
 elif [ "$device" = "2" ]; then
   . .$SNAP/devices/e5hd/e5hd.sh
 elif [ "$device" = "3" ]; then

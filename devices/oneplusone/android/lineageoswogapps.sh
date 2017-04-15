a401c96922aee85143cb36391be791a4c0a1ea4c
Marius Quabeck <marius.quabeck@ubuntu.com>
2017-04-16 01:59:20 +0200

    Fix Lineage OS download links for OnePlus One

--- devices/oneplusone/android/lineageoswogapps.sh
+++ devices/oneplusone/android/lineageoswogapps.sh
@@ -35,7 +35,7 @@ then
   echo "Downloading LineageOS 14.1.."
   echo ""
   sleep 1
-  wget -c --quiet --show-progress --tries=10 -P $HOME/.cache/magic-device-tool/ https://mirrorbits.lineageos.org/full/bacon/20170403/lineage-14.1-20170403-nightly-bacon-signed.zip
+  wget -c --quiet --show-progress --tries=10 -P $HOME/.cache/magic-device-tool/ https://mirrorbits.lineageos.org/full/bacon/20170410/lineage-14.1-20170410-nightly-bacon-signed.zip
   clear
   echo ""
   echo "Installing TWRP recovery"
@@ -65,13 +65,13 @@ then
   echo "Ignore that prompt, the tool will take care of the installation"
   echo ""
   echo "  → LineageOS 14.1 zip "
-  adb push -p $HOME/.cache/magic-device-tool/lineage-14.1-20170403-nightly-bacon-signed.zip /sdcard/
+  adb push -p $HOME/.cache/magic-device-tool/lineage-14.1-20170410-nightly-bacon-signed.zip /sdcard/
   echo ""
   sleep 1
   echo ""
   echo "Installing LineageOS.."
   echo ""
-  adb shell twrp install /sdcard/lineage-14.1-20170403-nightly-bacon-signed.zip
+  adb shell twrp install /sdcard/lineage-14.1-20170410-nightly-bacon-signed.zip
   sleep 1
   echo ""
   echo "Wipe cache.."

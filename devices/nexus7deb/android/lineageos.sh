b47e6a7a78f4e42f01ada2b4a896042a1e0cfe0b
Marius Quabeck <marius.quabeck@ubuntu.com>
2017-07-24 16:13:48 +0200

    Update Lineage OS for Nexus 7 deb

---
+++ devices/nexus7deb/android/lineageos.sh
@@ -0,0 +1,116 @@
+clear
+echo ""
+echo "Installing Lineage OS 14.1"
+echo ""
+sleep 1
+echo "Please boot your Nexus 7 into bootloader/fastboot mode by pressing Power & Volume Down (-)"
+echo ""
+sleep 1
+echo -n "Is your Nexus 7 in bootloader/fastboot mode now? [Y] "; read bootloadermode
+if [ "$bootloadermode"==Y -o "$bootloadermode"==y -o "$bootloadermode"=="" ]; then
+  clear
+  echo ""
+  echo "Detecting device"
+  echo ""
+  sleep 1
+  fastboot devices > /tmp/AttachedDevices
+fi
+if grep 'device$\|fastboot$' /tmp/AttachedDevices
+then
+  echo "Device detected !"
+  sleep 1
+  clear
+  fastboot format cache
+  fastboot format userdata
+  fastboot reboot-bootloader
+  sleep 6
+  clear
+  echo ""
+  echo "Downloading TWRP recovery"
+  echo ""
+  wget -c --quiet --show-progress --tries=10 -P $HOME/.cache/magic-device-tool/ http://mdt-files.com/downloads/magic-device-tool/recoverys/twrp-deb.img
+  sleep 1
+  echo ""
+  echo "Downloading Lineage OS 14.1.."
+  echo ""
+  sleep 1
+  wget -c --quiet --show-progress --tries=10 -P $HOME/.cache/magic-device-tool/ https://mirrorbits.lineageos.org/full/deb/20170717/lineage-14.1-20170717-nightly-deb-signed.zip
+  echo ""
+  echo "Downloading Open Gapps.."
+  echo ""
+  sleep 1
+  wget -c --quiet --show-progress --tries=10 -P $HOME/.cache/magic-device-tool/ http://mdt-files.com/downloads/magic-device-tool/gapps/open_gapps-arm-7.1-nano-20170603.zip
+  sleep 2
+  clear
+  echo ""
+  echo "Installing TWRP recovery"
+  echo ""
+  fastboot flash recovery $HOME/.cache/magic-device-tool/twrp-deb.img
+  sleep 1
+  echo ""
+  echo "Rebooting device.."
+  echo ""
+  echo "This will take ~35 seconds. Don't disconnect your device!"
+  echo ""
+  fastboot reboot-bootloader
+  sleep 7
+  fastboot boot $HOME/.cache/magic-device-tool/twrp-deb.img
+  sleep 15
+  adb reboot recovery
+  sleep 15
+  echo ""
+  clear
+  echo ""
+  echo "Pushing zip's to device"
+  sleep 1
+  echo ""
+  echo "Please wait this can take a while"
+  echo ""
+  echo "You may see a prompt asking you for read/write permissions"
+  echo "Ignore that prompt, the tool will take care of the installation"
+  echo ""
+  echo "  → Lineage OS 14.1 zip "
+  adb push -p $HOME/.cache/magic-device-tool/lineage-14.1-20170717-nightly-deb-signed.zip /sdcard/
+  echo ""
+  echo "  → gapps zip"
+  adb push -p $HOME/.cache/magic-device-tool/open_gapps-arm-7.1-nano-20170603.zip /sdcard/
+  echo ""
+  echo "========================================="
+  sleep 1
+  echo ""
+  echo "Installing Lineage OS 14.1.."
+  echo ""
+  adb shell twrp install /sdcard/lineage-14.1-20170717-nightly-deb-signed.zip
+  sleep 1
+  echo ""
+  echo "Installing GApps.."
+  echo ""
+  sleep 3
+  adb shell twrp install /sdcard/open_gapps-arm-7.1-nano-20170603.zip
+  echo ""
+  echo "Wipe cache.."
+  echo ""
+  adb shell twrp wipe cache
+  adb shell twrp wipe dalvik
+  echo ""
+  adb reboot
+  echo "The device is now rebooting. Give it time to flash the new ROM. It will boot on its own."
+  echo ""
+  sleep 5
+  echo ""
+  echo "Cleaning up.."
+  rm -f /tmp/AttachedDevices
+  echo ""
+  sleep 1
+  echo "Exiting script. Bye Bye"
+  sleep 1
+
+else
+  echo "Device not found"
+  rm -f /tmp/AttachedDevices
+  sleep 1
+  echo ""
+  echo "Back to menu"
+  sleep 1
+  . ./launcher.sh
+fi
---
+++ devices/nexus7deb/android/los.sh
@@ -0,0 +1,28 @@
+clear
+echo ""
+echo "Nexus 7 2013 (LTE) - deb"
+echo ""
+echo ""
+sleep 1
+echo "Cyanogenmod:"
+echo ""
+echo "[1]  Install Lineage OS 14.1"
+echo "[2]  Install Lineage OS 14.1 without GAPPS"
+echo ""
+echo ""
+echo "[3] Back to menu "
+echo ""
+echo -n "Enter option: "; read option
+if [ "$option" = "1" ]; then
+  . ./devices/nexus7deb/android/lineageos.sh
+elif [ "$option" = "2" ]; then
+  . ./devices/nexus7deb/android/lineageoswogapps.sh
+elif [ "$option" = "3" ]; then
+  . ./devices/nexus7deb/nexus7.sh
+else
+  echo ""
+  echo "Invalid Option"
+  echo ""
+  echo "Exiting script. Bye Bye"
+  exit
+fi
